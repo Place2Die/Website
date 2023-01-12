74db2b1390fd07258e8b6a058d8fc0cdee3a17d0
Axel ECKENBERG <axel.eckenberg@epitech.eu>
2023-01-12 15:34:08 +0100

    :sparkles: Add sentry

---
+++ plugins/sentry.ts
@@ -0,0 +1,36 @@
+import * as Sentry from '@sentry/vue'
+import { BrowserTracing } from '@sentry/tracing'
+
+export default defineNuxtPlugin((nuxtApp) => {
+    const config = useRuntimeConfig()
+
+    Sentry.init({
+        Vue: nuxtApp.vueApp,
+        dsn: config.SENTRY_DSN,
+        integrations: [
+            new BrowserTracing({
+                routingInstrumentation: Sentry.vueRouterInstrumentation(nuxtApp.router)
+            })
+        ],
+        tracesSampleRate: 1.0
+    })
+    nuxtApp.vueApp.config.errorHandler = (error, _vm, info) => {
+        Sentry.captureException(error, {
+            contexts: {
+                info: {
+                    info
+                }
+            }
+        })
+    }
+    nuxtApp.vueApp.config.warnHandler = (msg, _vm, trace) => {
+        Sentry.captureMessage(msg, {
+            contexts: {
+                info: {
+                    trace
+                }
+            }
+        })
+    }
+    nuxtApp.provide('sentry', Sentry)
+})
