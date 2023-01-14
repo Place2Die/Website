922d55480964efc66f4c69f4331f12cff199dfc2
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2023-01-14 17:25:55 +0100

    :sparkles: Add CSS for tooltips

--- app.vue
+++ app.vue
@@ -18,4 +18,39 @@ onMounted(async () => {
 html::-webkit-scrollbar {
     display: none;
 }
+
+/* Tooltip container */
+.tooltip {
+    display: inline-block;
+    position: relative;
+}
+
+/* Tooltip text */
+.tooltip .tooltip-text {
+    visibility: hidden;
+    opacity: 0;
+    width: 0;
+    height: 0;
+    background-color: #0d1117;
+    color: #fff;
+    text-align: center;
+    padding: 5px;
+    border-radius: 6px;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+    position: absolute;
+    transition: opacity 0.3s ease-in-out,
+                height 0.2s ease-in-out;
+    left: 110%;
+    top: -10%;
+}
+
+/* Show the tooltip text when you mouse over the tooltip container */
+.tooltip:hover .tooltip-text {
+    visibility: visible;
+    opacity: 1;
+    width: fit-content;
+    height: 2rem;
+}
 </style>
